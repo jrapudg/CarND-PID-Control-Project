307d87c2b72a2be9abfece6bfa327f87670e1a44
Juan-Alvarez-Padilla <jrap.udg@gmail.com>
2019-03-25 22:12:39 -0600

    PID.cpp and main.cpp finished, initialization functions and methods of pid

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,6 +12,13 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
+   Kp = Kp_;
+   Ki = Ki_;
+   Kd = Kd_;
+
+   p_error = 0;
+   i_error = 0;
+   d_error = 0;
 
 }
 
@@ -19,6 +26,9 @@ void PID::UpdateError(double cte) {
   /**
    * TODO: Update PID errors based on cte.
    */
+   d_error = cte - p_error;
+   p_error = cte;
+   i_error += cte;
 
 }
 
@@ -26,5 +36,12 @@ double PID::TotalError() {
   /**
    * TODO: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+   double steer_value = (Kp * p_error) + (Ki * i_error) + (Kd * d_error);
+   if (steer_value < -1){
+     steer_value = -1;
+   }else if (steer_value > 1){
+     steer_value = 1;
+   }
+
+  return steer_value;  // TODO: Add your total error calc here!
 }
--- src/main.cpp
+++ src/main.cpp
@@ -37,8 +37,14 @@ int main() {
   /**
    * TODO: Initialize the pid variable.
    */
+   double Kp_initial = -0.091;
+   double Ki_initial = -0.0005;
+   double Kd_initial = -1.693;
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
+   pid.Init(Kp_initial, Ki_initial, Kd_initial);
+
+
+  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -63,9 +69,17 @@ int main() {
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
+
+           //Update the error in system using cte.
+          pid.UpdateError(cte);
+
+          //Derive the steering angle in order to minimize the error (or cte).
+          steer_value = pid.TotalError();
+
           
+
           // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
+          std::cout << "CTE: " << cte << " Steering Value: " << steer_value
                     << std::endl;
 
           json msgJson;
@@ -87,7 +101,7 @@ int main() {
     std::cout << "Connected!!!" << std::endl;
   });
 
-  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, 
+  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
                          char *message, size_t length) {
     ws.close();
     std::cout << "Disconnected" << std::endl;
@@ -100,6 +114,6 @@ int main() {
     std::cerr << "Failed to listen to port" << std::endl;
     return -1;
   }
-  
+
   h.run();
 }
